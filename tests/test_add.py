c89208d2074f29c3c38a15e747596bba10a4e8e2
Massiwen <massiwen@outlook.com>
2021-10-29 20:37:34 -0400

    Fix my test

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,7 +3,7 @@ import pytest
 from main.MyClass import MyClass
 
 
-@pytest.mark.parametrize('a,b', [(2, 3), (3, 4), (3, 5), (5, 9)])
+@pytest.mark.parametrize('a,b', [(2, 3), (3, 4), (4, 5), (8, 9)])
 def test_add(a, b):
     mc = MyClass()
 

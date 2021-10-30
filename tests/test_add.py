dd1bfcea9606d76b800d8a19aeac6e687d25f71c
Massiwen <massiwen@outlook.com>
2021-10-29 20:38:33 -0400

    Change 2

--- tests/test_add.py
+++ tests/test_add.py
@@ -6,6 +6,7 @@ from main.MyClass import MyClass
 @pytest.mark.parametrize('a,b', [(2, 3), (3, 4), (4, 5), (8, 9)])
 def test_add(a, b):
     mc = MyClass()
-    # 
+    #
+    # wd
 
     assert b == mc.add(1, a)
